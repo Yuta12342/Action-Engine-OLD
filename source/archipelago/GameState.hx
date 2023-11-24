4b69f39cb9592effca85e9d191be5732832f96ca
Z11Coding/Gaming <Zanezack1000@gmail.com>
2023-11-24 02:19:19 -0500

    im going to bed

--- source/archipelago/GameState.hx
+++ source/archipelago/GameState.hx
@@ -2,8 +2,19 @@ package archipelago;
 
 import flixel.FlxState;
 import ap.Client;
+import ap.PacketTypes.ClientStatus;
+import ap.PacketTypes.NetworkItem;
 
 class GameState extends FlxState {
 
+    public function new(ap:Client, slotData:Dynamic)
+    {
+        _ap = ap;
+        _ap.clientStatus = ClientStatus.READY;
+        _ap._hOnItemsReceived = onItemsReceived;
+        _ap._hOnSocketDisconnected = onSocketDisconnect;
+
+        super();
+    }
 
 }
